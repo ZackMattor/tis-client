f9b80fcc7ce040f4a9708b7eb5eba56342157bb3
Zack Mattor <zmattor@gmail.com>
2016-11-11 01:08:08 -0500

    copy html file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp');
 const babel = require('babel-loader');
 var webpack = require('webpack-stream');
+var merge = require('merge-stream');
 
 gulp.task('default', () => {
-  return gulp.src('src/**/*.js')
+  var javascript =  gulp.src('src/**/*.js')
     .pipe(webpack({
       watch: true,
       output: {
-        filename: 'foo.js'
+        filename: 'app.js'
       },
       module: {
         loaders: [
@@ -23,4 +24,9 @@ gulp.task('default', () => {
       }
     }))
     .pipe(gulp.dest('dist'));
+
+  var html = gulp.src('src/**/*.html')
+    .pipe(gulp.dest('dist'));
+
+  return merge(javascript, html)
 });
