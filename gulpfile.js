458ec594429a2b2a857b36ba3c45b1c9ba5f602b
Zack Mattor <zmattor@gmail.com>
2016-11-11 20:39:58 -0500

    implementing webpack-dev-server

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,73 @@
 const gulp = require('gulp');
-const babel = require('babel-loader');
-var webpack = require('webpack-stream');
-var merge = require('merge-stream');
-var watch = require('gulp-watch');
+const webpack = require('webpack');
+const merge = require('merge-stream');
+const watch = require('gulp-watch');
+const WebpackDevServer = require("webpack-dev-server");
+const gutil = require("gulp-util");
+const path = require("path");
+
+var webpack_config = {
+  entry: {
+    app: ["./src/index.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/assets/',
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
 
 gulp.task('default', () => {
   var javascript =  gulp.src('src/**/*.js')
-    .pipe(webpack({
-      watch: true,
-      output: {
-        filename: 'app.js'
-      },
-      module: {
-        loaders: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel', // 'babel-loader' is also a valid name to reference
-            query: {
-              presets: ['es2015']
-            }
-          }
-        ]
-      }
-    }))
-    .pipe(gulp.dest('dist'));
+                        .pipe(webpack())
+                        .pipe(gulp.dest('dist'));
 
   var html = watch('src/**/*.html', function() {
     gulp.src('src/**/*.html')
-      .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'));
   });
 
   return merge(javascript, html);
 });
+
+
+gulp.task("webpack-dev-server", function(callback) {
+  // Start a webpack-dev-server
+  var compiler = webpack(webpack_config);
+
+  var html = watch('src/**/*.html', function() {
+    gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist'));
+  });
+
+  new WebpackDevServer(compiler, {
+    publicPath: webpack_config.output.publicPath,
+    stats: {
+      colors: true
+    }
+  }).listen(4200, "localhost", function(err) {
+    if(err) throw new gutil.PluginError("webpack-dev-server", err);
+    // Server listening
+    gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+
+    // keep the server alive or continue?
+    // callback();
+  });
+});
+
+function onError(err) {
+  console.log(err);
+  this.emit('end');
+}
